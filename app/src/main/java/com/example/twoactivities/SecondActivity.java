63ddb9f23b583f176e20acc4dec360a1691783bf
RohanSubba <rohan.ronaldo07.rs@gmail.com>
2021-03-16 23:10:53 +0530

    coding challenge completed

--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -6,69 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "ca.nbcc.shoppinglist.extra.MESSAGE";
 
-    public static final String EXTRA_REPLY =
-            "com.example.android.twoactivities.extra.REPLY";
 
-    private EditText mReply;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
-        mReply = findViewById(R.id.editText_second);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(LOG_TAG,"onStart");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(LOG_TAG, "onPause");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(LOG_TAG, "onRestart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(LOG_TAG, "onResume");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(LOG_TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(LOG_TAG, "onDestroy");
-    }
-
-    public void returnReply(View view) {
-        String reply = mReply.getText().toString();
+    public void addItem(View view) {
         Intent replyIntent = new Intent();
-        replyIntent.putExtra(EXTRA_REPLY, reply);
-        setResult(RESULT_OK,replyIntent);
-        Log.d(LOG_TAG, "End SecondActivity");
+        String message = ((Button) view).getText().toString();
+        replyIntent.putExtra(EXTRA_MESSAGE, message);
+        setResult(RESULT_OK, replyIntent);
         finish();
     }
 }
